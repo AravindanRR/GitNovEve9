5b9c4c0ecd917f3a3b5f3184f69e168f55535397
AravindanRR <rraravindan22@gmail.com>
2021-11-10 21:02:21 +0530

    sweet

---
+++ src/org/ware/Snow.java
@@ -0,0 +1,15 @@
+package org.ware;
+
+public class Snow {
+	
+	private void empName() {
+		System.out.println("Aravindan");
+		
+	}
+	public static void main(String[] args) {
+		Snow s = new Snow();
+		s.empName();
+	}
+
+	
+}
