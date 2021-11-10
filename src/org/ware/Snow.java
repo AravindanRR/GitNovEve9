9a9a146e96145c12dcf8a11e45a664e844435a7e
AravindanRR <rraravindan22@gmail.com>
2021-11-10 21:28:42 +0530

    successful

--- src/org/ware/Snow.java
+++ src/org/ware/Snow.java
@@ -4,11 +4,15 @@ public class Snow {
 	
 	private void empName() {
 		System.out.println("Aravindan");
-		
+	}
+	private void empAdd() {
+		System.out.println("karapakam");
+
 	}
 	public static void main(String[] args) {
 		Snow s = new Snow();
 		s.empName();
+		s.empAdd();
 	}
 
 	
